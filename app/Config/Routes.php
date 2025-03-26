771d2c01e8060020bf01b7ca27a6737c69735148
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-26 11:12:18 +0700

    login register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/register', 'AuthController::register');
+$routes->post('/register/store', 'AuthController::store');
+$routes->get('/login', 'AuthController::login');
+$routes->post('/auth', 'AuthController::auth');
+$routes->get('/logout', 'AuthController::logout');
+
